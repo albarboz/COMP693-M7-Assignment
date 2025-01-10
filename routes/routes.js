39f6ccb70d03af862f8595d7f24842080c75be1d
Al <115059113+albarboz@users.noreply.github.com>
2025-01-09 16:57:21 -0800

    Initial Commit

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { getAllEmployees, createEmployee, deleteEmployee, getEmployee, updateEmployee } from '../controllers/employee.js'
+
+const router = express.Router()
+
+router.route('/api/employees')
+    .get(getAllEmployees)
+    .post(createEmployee)
+
+router.route('/api/employees/:id')
+    .delete(deleteEmployee)
+    .patch(updateEmployee)
+    .get(getEmployee)
+
+
+export default router 
